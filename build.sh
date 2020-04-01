eee8bc08fa33a45e404a8fa83647f37331c3eac8
william.morrison <william.morrison@angieslist.com>
2020-04-01 14:15:54 -0400

    Changed cat to < for name

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -eux
 # Docker and Git Hub username are assumed to be the same and is pulled from environment variable
 export USERNAME="$HUBUSERNAME"
 # Image name
-export IMAGE="$(cat NAME)"
+export IMAGE="$(< NAME)"
 echo "$USERNAME/$IMAGE:latest"
 docker build --force-rm --no-cache -t "$USERNAME/$IMAGE:latest" .
