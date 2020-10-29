1ad6b4cb9314c0ed0cccfe8f6f3f52783824d790
william.morrison <william.morrison@angieslist.com>
2020-10-29 13:13:55 -0400

    Need to make sure authentication token is current

--- buildaws.sh
+++ buildaws.sh
@@ -4,6 +4,7 @@ set -eux
 REPONAME="894680052389.dkr.ecr.us-east-1.amazonaws.com"
 export AWS_PAGER=""
 aws sts get-caller-identity | grep '894680052389' && echo 'Yes aws sso logged in' || echo "Need to run > aws sso login"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${REPO}"
 # Image name and version are pulled from files in the build directory: VERSION and NAME
 export version=$(< VERSION)
 export IMAGE="$(< NAME)"
