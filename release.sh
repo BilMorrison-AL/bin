79ff6dbd5e2d39be99c0681a4503b0a31cada7e1
william.morrison <william.morrison@angieslist.com>
2020-11-04 18:49:15 -0500

    A bunch of changes, to many to mention ¯\_(ツ)_/¯

--- release.sh
+++ release.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eux
-# REPONAME is either set to the organization name or your username
-export REPONAME="angieslist"
-#export REPONAME="williammorrisonal"
+# REPO is either set to the organization name or your username
+export REPO="angieslist"
+#export REPO="williammorrisonal"
 # Always make sure it is current
+echo '---'
 git pull
 # Image name and version are pulled from files in the build directory: VERSION and NAME
 version="$(< VERSION)"
-IMAGE="$(< NAME)"
+name="$(< NAME)"
 # Increment the minor version by one for every release
 echo "Old Version:    $version"
-docker images | grep "$IMAGE" | grep "$version" | awk '{print $3}'| xargs -I {} docker rmi -f {}
+docker images | grep "$name" | grep "$version" | awk '{print $3}'| xargs -I {} docker rmi -f {}
 oldminornum=$(cut -d '.' -f3 VERSION)
 newminornum=$(( $oldminornum + 1 ))
 #   $((..)), ${} or [[ ]]
-sed -iu "s/[0-9]\.[0-9]\.$oldminornum/1\.1\.$newminornum/" VERSION
+gsed -i "s/[0-9]\.[0-9]\.$oldminornum/1\.1\.$newminornum/" VERSION
 #sed -iu "s/$oldminornum/$newminornum/g"  VERSION
-rm -f VERSIONu
+#rm -f VERSIONu
 version=$(< VERSION)
-echo "New Version:    $version"
-echo "IMAGE  $IMAGE"
+echo "Org-User Name  -  Image Name   -  Version number:    ${REPO}/${name}:${version}"
 # Run build - Docker/Git Hub Username are defined in build
-docker build --force-rm --no-cache -t "$REPONAME/$IMAGE:latest" .
-docker build --force-rm --no-cache -t "$REPONAME/$IMAGE:$version" .
-
-echo "Org-User Name  -  Image Name   -  Version number:    $REPONAME/$IMAGE:$version"
-docker tag  "$IMAGE:$version"  "$REPONAME/$IMAGE:$version"
-docker tag  "$IMAGE:$version"  "$REPONAME/$IMAGE:latest"
+docker build --force-rm --no-cache -t "${REPO}/${name}:${version}" .
+echo ''
+echo "Org - Image - Version"
+echo "${REPO}/${name}:${version}"
+docker tag  "${REPO}/${name}:${version}"  "${REPO}/${name}:${version}"
+docker tag  "${REPO}/${name}:latest"  "${REPO}/${name}:latest"
 #
 git add -A
 git commit -m "Version $version"
@@ -34,7 +34,5 @@ git commit -m "Version $version"
 git tag -a "$version" -m "Version $version"
 git push
 git push --tags
-echo "version   $version"
-# Push it to dockerhub
-docker push "$REPONAME/$IMAGE:$version"
-docker push "$REPONAME/$IMAGE:latest"
+echo 'Push it to dockerhub'
+docker push "${REPO}/${name}:latest"
--- releaseaws.sh
+++ releaseaws.sh
@@ -1,15 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eux
+echo '---'
+PATH="${HOME}/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 # Repo name for aws not using Dockerhub
 REPO="894680052389.dkr.ecr.us-east-1.amazonaws.com"
+echo '---'
 # Always make sure it is current
-git pull
-# Make sure connected to AWS
+ALPROFILE=' --profile sysops-permission-set-894680052389 '
+# Pull down any changes from remote origin
+## git pull
+echo "AWS Version"
+aws ${ALPROFILE}--version
 export AWS_PAGER=""
-aws --version
+# Make sure connected to AWS
+#   "${AL}"
 #$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
-aws sts get-caller-identity | grep '894680052389' && echo 'Yes aws sso logged in' || echo "Need to run > aws sso login"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${REPO}"
+#aws sts get-caller-identity | grep '894680052389' && echo 'Yes aws sso logged in' || echo "Need to run > aws sso login"
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${REPO}"
+
+# If this fails, then manually run -> aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 894680052389.dkr.ecr.us-east-1.amazonaws.com
+
+#AWSTOKEN="$(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${REPO})"
+echo ''
+#echo "$AWSTOKEN" | docker login -u AWS --password-stdin "$REPO"
+
 # Image name and version are pulled from files in the build directory: VERSION and NAME
 version="$(< VERSION)"
 name="$(< NAME)"
@@ -27,21 +41,27 @@ echo "New Version  ${name}   :  ${version}"
 echo ''
 # Run build - Docker/Git Hub Username are defined in build
 #docker build --force-rm --no-cache -t "${REPO}/${name}:${version}" .
-docker build --force-rm --no-cache -t "${name}:latest" .
+docker build --force-rm --no-cache -t "${REPO}/${name}:${version}" .
 echo ''
 echo "Org - Image - Version"
 echo "${REPO}/${name}:${version}"
+
 #docker tag "$name:$version" "${REPO}/${name}:${version}"
-docker tag "${name}":latest "${REPO}/${name}":latest
-## git add -A
-## git commit -m "Version ${version}"
+#docker tag "${name}":latest "${REPO}/${name}":latest
+docker tag "${REPO}/${name}":${version} "${REPO}/${name}:${version}"
+docker tag "${REPO}/${name}":latest "${REPO}/${name}:latest"
+
+echo ''
+echo 'Git add commit push'
+git add -A
+git commit -m "Version ${version}"
 # Tag it
-## git tag -a "${version}" -m "Version ${version}"
-## git push
-## git push --tags
+git tag -a "${version}" -m "Version ${version}"
+git push
+git push --tags
 
 echo "Push Container Image  ${version}  to  ${REPO}"
-# Push it to dockerhub
+echo 'Push it to ECR'
 # docker push "${REPO}/${name}:${version}"
 docker push "${REPO}/${name}":latest
 #
